4d847b2afbb2f57839feae3ad6e82fbc71479af9
Amir Rachum <amir@rachum.com>
2015-07-20 20:13:13 +0300

    Bump version to 0.6.0

--- pep257.py
+++ pep257.py
@@ -59,7 +59,7 @@ except AttributeError:
     tokenize_open = open
 
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 __all__ = ('check', 'collect')
 
 PROJECT_CONFIG = ('setup.cfg', 'tox.ini', '.pep257')
