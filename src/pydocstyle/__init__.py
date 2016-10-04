023f5d95eb7482570cfbbd3c3d624c024d10aa17
Amir Rachum <amir@rachum.com>
2016-10-04 13:12:03 +0300

    Export symbols for flake8-docstrings to work.

--- src/pydocstyle/__init__.py
+++ src/pydocstyle/__init__.py
@@ -1,1 +1,8 @@
 from .checker import check
+from .violations import Error, conventions
+from .utils import __version__
+
+# Temporary hotfix for flake8-docstrings
+from .checker import PEP257Checker, tokenize_open
+from .parser import AllError
+
