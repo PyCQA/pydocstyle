2e667fbffc56129a640c2dbd675aa48d8c1ec3cf
Amir Rachum <amir@rachum.com>
2017-10-08 11:56:16 +0300

    Bump version: 2.0.1rc → 2.1.0rc

--- src/pydocstyle/utils.py
+++ src/pydocstyle/utils.py
@@ -8,7 +8,7 @@ except ImportError:
 
 
 # Do not update the version manually - it is managed by `bumpversion`.
-__version__ = '2.0.1rc'
+__version__ = '2.1.0rc'
 log = logging.getLogger(__name__)
 
 
