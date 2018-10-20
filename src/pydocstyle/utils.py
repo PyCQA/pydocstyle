4698db0869a758ae008a8a4a189d292036e59874
Shachar Ohana <shachar.o@gmail.com>
2018-10-20 10:09:24 +0300

    Add types to utils.py

--- src/pydocstyle/utils.py
+++ src/pydocstyle/utils.py
@@ -1,6 +1,7 @@
 """General shared utilities."""
 import logging
 from itertools import tee
+from typing import Iterable, Any, Tuple
 try:
     from itertools import zip_longest
 except ImportError:
@@ -8,16 +9,19 @@ except ImportError:
 
 
 # Do not update the version manually - it is managed by `bumpversion`.
-__version__ = '3.0.1rc'
+__version__: str = '3.0.1rc'
 log = logging.getLogger(__name__)
 
 
-def is_blank(string):
+def is_blank(string: str) -> bool:
     """Return True iff the string contains only whitespaces."""
     return not string.strip()
 
 
-def pairwise(iterable, default_value):
+def pairwise(
+    iterable: Iterable,
+    default_value: Any,
+) -> Iterable[Tuple[Any, Any]]:
     """Return pairs of items from `iterable`.
 
     pairwise([1, 2, 3], default_value=None) -> (1, 2) (2, 3), (3, None)
