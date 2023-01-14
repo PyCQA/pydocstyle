801fc92acb0f501d68fb9dea5e4ec4a5d8050ff6
Felipe Peter <mr-peipei@web.de>
2023-01-14 11:01:33 +0100

    Add Convention class

---
+++ src/pydocstyle/conventions.py
@@ -0,0 +1,109 @@
+"""This module contains the convention definitions."""
+
+
+from typing import Literal, Set
+
+from pydocstyle.violations import all_errors
+
+CONVENTION_NAMES = ("pep257", "numpy", "google")
+
+
+convention_errors = {
+    'pep257': all_errors
+    - {
+        'D203',
+        'D212',
+        'D213',
+        'D214',
+        'D215',
+        'D404',
+        'D405',
+        'D406',
+        'D407',
+        'D408',
+        'D409',
+        'D410',
+        'D411',
+        'D413',
+        'D415',
+        'D416',
+        'D417',
+        'D418',
+    },
+    'numpy': all_errors
+    - {
+        'D107',
+        'D203',
+        'D212',
+        'D213',
+        'D402',
+        'D413',
+        'D415',
+        'D416',
+        'D417',
+    },
+    'google': all_errors
+    - {
+        'D203',
+        'D204',
+        'D213',
+        'D215',
+        'D400',
+        'D401',
+        'D404',
+        'D406',
+        'D407',
+        'D408',
+        'D409',
+        'D413',
+    },
+}
+
+
+class Convention:
+    """This class defines the convention to use for checking docstrings."""
+
+    def __init__(
+        self, name: Literal["pep257", "numpy", "google"] = "pep257"
+    ) -> None:
+        """Initialize the convention.
+
+        The convention has two purposes. First, it holds the error codes to be
+        checked. Second, it defines how to treat docstrings, eliminating the
+        need for extra logic to determine whether a docstring is a NumPy or
+        Google style docstring.
+
+        Each convention has a set of error codes to check as a baseline.
+        Specific error codes can be added or removed via the
+        :code:`add_error_codes` or :codes:`remove_error_codes` methods.
+
+        Args:
+            name (Literal["pep257", "numpy", "google"], optional): The convention to use. Defaults to "pep257".
+
+        Raises:
+            ValueError: _description_
+        """
+        if name not in CONVENTION_NAMES:
+            raise ValueError(
+                f"Convention '{name}' is invalid. Must be one "
+                f"of {CONVENTION_NAMES}."
+            )
+
+        self.name = name
+        self.error_codes = convention_errors[name]
+
+    def add_error_codes(self, error_codes: Set[str]) -> None:
+        """Add additional error codes to the convention.
+
+        Args:
+            error_codes (Set[str]): The error codes to also check.
+        """
+        self.error_codes = self.error_codes.union(error_codes)
+
+    def remove_error_codes(self, error_codes: Set[str]) -> None:
+        """Remove error codes from the convention.
+
+        Args:
+            error_codes (Set[str]): The error codes to ignore.
+        """
+        self.error_codes = self.error_codes - error_codes
---
+++ src/tests/test_conventions.py
@@ -0,0 +1,78 @@
+"""This module contains tests for the available conventions."""
+
+import re
+
+import pytest
+
+from pydocstyle.conventions import CONVENTION_NAMES, Convention
+
+
+def test_only_specific_convention_names_are_allowed() -> None:
+    """Test that an error is raised if an invalid convention name is used."""
+    with pytest.raises(
+        ValueError, match="Convention 'invalid_convention' is invalid"
+    ):
+        convention = Convention("invalid_convention")
+
+
+def test_default_convention_is_pep257() -> None:
+    """Test that pep257 is used as the default convention."""
+    convention = Convention()
+
+    assert convention.name == "pep257"
+
+
+@pytest.mark.parametrize("convention_name", CONVENTION_NAMES)
+def test_names_are_set_correctly(convention_name: str) -> None:
+    """Test that the convention holds its name as an attribute."""
+    convention = Convention(convention_name)
+
+    assert convention.name == convention_name
+
+
+@pytest.mark.parametrize("convention_name", CONVENTION_NAMES)
+def test_conventions_keep_their_error_codes_as_attribute(
+    convention_name: str,
+) -> None:
+    """Check that conventions are initialized with a set of error codes."""
+    convention = Convention(convention_name)
+
+    assert len(convention.error_codes) > 0
+
+    for error_code in convention.error_codes:
+        assert len(error_code) == 4
+        assert re.compile(r"D[1-4]\d\d").match(error_code)
+
+
+def test_can_add_error_codes() -> None:
+    """Test that additional error codes can be added to a convention."""
+    convention = Convention()
+
+    n_errors_before_adding = len(convention.error_codes)
+
+    assert "D203" not in convention.error_codes
+    assert "D212" not in convention.error_codes
+
+    convention.add_error_codes({"D203", "D212"})
+
+    assert len(convention.error_codes) - n_errors_before_adding == 2
+
+    assert "D203" in convention.error_codes
+    assert "D212" in convention.error_codes
+
+
+def test_can_remove_error_codes() -> None:
+    """Test that specific error codes can be removed from a convention."""
+    convention = Convention()
+
+    n_errors_before_removal = len(convention.error_codes)
+
+    assert "D100" in convention.error_codes
+    assert "D102" in convention.error_codes
+
+    convention.remove_error_codes({"D100", "D102"})
+
+    assert n_errors_before_removal - len(convention.error_codes) == 2
+
+    assert "D100" not in convention.error_codes
+    assert "D102" not in convention.error_codes
